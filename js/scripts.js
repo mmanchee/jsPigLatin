9ec3d93e46fe3deb25ab4eb651a59a1e749ad769
Mike Manchee <mikemanchee@gmail.com>
2020-08-19 16:09:24 -0700

    add js functions to the project
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,45 @@
+function isVowel(input){
+  let word = input.join(" ");
+  word = word + "way";
+  return word;
+}
+
+function isConsonant(word) {
+  const wordArray = word.split('');
+  for (i = 0; i < wordArray.length; i++) {
+    if (/[^aeiou]/i.test(wordArray[0])) {
+      if (wordArray[0] == "q") {
+        if (wordArray[1] == "u") {
+          wordArray.push(wordArray[0]);
+          wordArray.shift();
+        }
+      }
+        wordArray.push(wordArray[0]);
+        wordArray.shift();
+    } else {
+      word = wordArray.join("");
+      word = word + "ay"; 
+      return word;
+    }
+  }
+}
+
+function pigLatin(input) {
+  let inputArray = input.split(" ");
+  for (let i = 0; i < inputArray.length; i++) {
+    letters = inputArray[i].split("");
+    if (/[aeiou]/i.test(letters[0])) {
+      inputArray[i] = isVowel(letters);
+    } else if (/[a-z]/i.test(letters[0])) {
+      inputArray[i] = isConsonant(Letters);
+    } else {
+      inputArray[i] = inputArray[i];
+    }
+  }; 
+  input = inputArray.join(" ");
+  return input;
+}
+
 $(document).ready(function(){
   $("#test-input1").submit(function(event) {
     event.preventDefault();
@@ -12,24 +54,12 @@ $(document).ready(function(){
     };
     $("#result1").text(number1);
   });
+  $("test-input2").submit(function(event) {
+    event.preventDefault();
+    let input2 = $("input#input2").val();
+
+    let result2 = pigLatin(input2);
+
+    $("#result2").text(result2);
+  });
 });
-
-// any word that begins with a vowel, add "way" to the end.
-
-// input: away
-// output: awayway
-
-// for consonants move first consonants to the end and ad "ay"
-
-// input: carefully
-// output: arefullycay
-
-// for 'qu' treat it as a consonant.
-
-// input: squirl
-// output: irlsquay
-
-// for 'y' treat it as a consonant.
-
-// input: yellow
-// output: ellowyay
