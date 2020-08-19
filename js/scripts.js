50f402fb969b33bc278a9e8c651082130a484033
Mike Manchee <mikemanchee@gmail.com>
2020-08-19 10:56:37 -0700

    add basic structure to project
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+const regex = /[aeiou]/;
+
+let input = $("input#input1").val();
+
+const array = input.match(/[aeiou]/g);
+
+console.log(array.length);
+
+
+//const array = input.match("a","i","e","o", "u");
+//console.log(array.length);
+
+//const array = input.match();
