623173c8de277043912fd757363d6c2c6ef910c0
Mike Manchee <mikemanchee@gmail.com>
2020-08-19 11:43:45 -0700

    debug js structure to project
    Co-authored-by: Alex Goodwin <hagoodwi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,4 @@
-const regex = /[aeiou]/;
 
-let input = $("input#input1").val();
-
-const array = input.match(/[aeiou]/g);
-
-console.log(array.length);
 
 
 //const array = input.match("a","i","e","o", "u");
@@ -12,4 +6,19 @@ console.log(array.length);
 
 //const array = input.match();
 
-//Co-authored-by: Alex Goodwin <hagoodwi@gmail.com> 
+
+$(document).ready(function(){
+  $("#test-input1").submit(function(event) {
+    event.preventDefault();
+    let input1 = $("input#input1").val();
+    const array1 = input1.match(/[aeiou]/gi);
+    let number1 = 0;
+
+    if (array1 == null) {
+      number1 = 0;
+    } else {
+      number1 = array1.length;
+    };
+    $("#result1").text(number1);
+  });
+});
